60c0ef9fbb333062b237eaef40cb0179440bbc88
谭晓珊 <tanxiaoshan@weimiao.cn>
2022-03-20 12:06:32 +0800

    update

---
+++ test/rocketmq_test.go
@@ -0,0 +1,21 @@
+/**
+ * @Author: shershon
+ * @Description:
+ * @Date: 2022/03/18 9:42 PM
+ */
+
+package test
+
+import (
+	"shershon1991/go-study-example/app/rocketmqdemo/consumer"
+	"shershon1991/go-study-example/app/rocketmqdemo/producer"
+	"testing"
+)
+
+func TestProducer(t *testing.T) {
+	producer.Simple()
+}
+
+func TestConsumer(t *testing.T) {
+	consumer.Simple()
+}
